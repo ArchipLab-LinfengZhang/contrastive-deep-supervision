03a6ceb58ef531c92d0573072b3324c7af55bbdc
ArchipLab-LinfengZhang <53758983+ArchipLab-LinfengZhang@users.noreply.github.com>
2022-07-13 13:07:10 +0800

    Add files via upload

--- ImageNet/KD_Contrastive_Deep_Supervision/run.sh
+++ ImageNet/KD_Contrastive_Deep_Supervision/run.sh
@@ -1,1 +1,2 @@
-sa
+#Note that before running this command, you should first train a ResNet50 teacher on ImageNet.
+python main.py -a resnet18 --dist-url 'tcp://127.0.0.1:20401' --dist-backend 'nccl' --multiprocessing-distributed --world-size 1 --rank 0 --resume checkpoint.pth.tar  /home/lthpc/datasets/ImageNet
